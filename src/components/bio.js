3eb4ed5470b64f94af9a886ae4cc93a6d43d4355
hadpeter <hadpeter@umich.edu>
2020-05-11 22:18:04 -0400

    site loads but no style

--- src/components/bio.js
+++ src/components/bio.js
@@ -6,6 +6,7 @@ class Bio extends Component{
             <h1>
                 Hadley Peterson
             </h1>
+            <meta charset="utf-8"></meta>
             <p>
             I'm a rising Senior at the University of Michigan,  Ann Arbor, majoring in Computer Science. Outside of coursework I enjoy contributing to several student organizations, meditating, and practicing yoga.
             </p>
