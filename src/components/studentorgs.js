5a21c0f58627eee763d8416ae7b9a6792c1820f6
hadpeter <hadpeter@umich.edu>
2020-05-16 22:39:55 -0400

    styling for student orgs

--- src/components/studentorgs.js
+++ src/components/studentorgs.js
@@ -3,85 +3,82 @@ import React, { Component } from "react";
 class Studentorgs extends Component{
     render(){
         return <section id = "studentorgs">
-            <head class="resume-section p-3 p-lg-5 d-flex align-items-center">
+            <h1 class="resume-section p-3 p-lg-5 d-flex align-items-center">
                 Student Organizations
-            </head>
-            <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-            <div class="resume-content">
-                <h3 class="mb-0">Fellow and Mentor</h3>
-                <div class="subheading mb-3">UpRound Ventures</div>
-                <p>
-                UpRound is a student organization that educates the community on venture capital and evaluates and sources deals as a scout fund for Detroit Venture Partners, Plug n Play Ventures, and other firms in Southeast Michigan.
-                </p>
-                <ul>
-                <li>
-                    Fostered education on venture capital by hosting community speaker events, networking sessions, and market update discussions
-                </li>
-                <li>
-                    Selected to partake in market discussion and analyze what market sectors are promising for deal sourcing. In total, we have sourced 11 deals to our partnered VCs.
-                </li>
-                <li>
-                    Hosted speaker events to educate the Ann Arbor community about venture capital and network directly with successful venture capitalists and entrepreneurs
-                </li>
-                </ul>
-            </div>
-            <div class="resume-date text-md-right">
-                <span class="text-primary">October 2018 - Present</span>
-            </div>
-            </div>
-
-            <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-            <div class="resume-content">
-                <h3 class="mb-0">Head of Recruitment</h3>
-                <div class="subheading mb-3">Sigma Eta Pi</div>
-                <p>
-                Sigma Eta Pi is the first national professional fraternity that focuses on technology focused entrepreneurship.
-                </p>
-                <ul>
-                <li>
-                    Marketed our rush, networking and speaker events to strengthen our presence on campus through flyers and social media
-                </li>
-                <li>
-                    Organized a startup trek for 40 students. This involved cultivating relationships with 20 startups, and handling travel logistics from Ann Arbor to Chicago for two nights
-                </li>
-                <li>
-                    Collaborated and led the entrepreneurial committee to host speakers and hold ideation sessions to increase interest and knowledge in entrepreneurship
-                </li>
-                </ul>
-            </div>
-            <div class="resume-date text-md-right">
-                <span class="text-primary">November 2019 - April 2020</span>
-            </div>
+            </h1>
+            <div class="resume-section p-3 p-lg-5 d-flex align-items-center">
+                <div class="text-left">
+                    <h2>Fellow and Mentor</h2>
+                    <h class = "text-muted">UpRound Ventures<br></br>
+                    October 2018 - Present
+                    </h>
+                    <p class = "text-muted">
+                    UpRound is a student organization that educates the community on venture capital and evaluates and sources deals as a scout fund for Detroit Venture Partners, Plug n Play Ventures, and other firms in Southeast Michigan.
+                    </p>
+                    <ul class = "text-muted">
+                        <li>
+                            Fostered education on venture capital by hosting community speaker events, networking sessions, and market update discussions
+                        </li>
+                        <li>
+                            Selected to partake in market discussion and analyze what market sectors are promising for deal sourcing. In total, we have sourced 11 deals to our partnered VCs.
+                        </li>
+                        <li>
+                            Hosted speaker events to educate the Ann Arbor community about venture capital and network directly with successful venture capitalists and entrepreneurs
+                        </li>
+                    </ul>
+                </div>
             </div>
 
-            <div class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-            <div class="resume-content">
-                <h3 class="mb-0">Makeathon Director and VP Corporate Relations</h3>
-                <div class="subheading mb-3">MPowered Entrepreneurship</div>
-                <p>
-                Makeathon is a 36-hour long product design competition that empowers students with the mentorship and materials needed to create impactful and innovative products
-                </p>
-                <ul>
-                <li>
-                    Recruited and led a team of nine students to organize, finance, and market the entire event
-                </li>
-                <li>
-                    Sourced and secured sponsorships in excess of $25,000 from 15+ sponsors including Autodesk, National Instruments and General Electric
-                </li>
-                <li>
-                    Designed and implemented a case competition, balancing rigor and flexibility to maximize participant engagement. The case is centered around the automotive industry and specifically autonomous vehicles. 
-                </li>
-                <li>
-                    Created and put into motion marketing initiatives to attract students of all undergraduate years, as well as master students, through in-person marketing, social media, and press releases
-                </li>
-                <li>
-                    Created an interdisciplinary prize to encourage cross-school collaboration and diversification of event participants and products created. Historically, Makeathon has suffered from a lack of representation across schools; 73% of our participants last year were from the college of engineering.
-                </li>
-                </ul>
+            <div class="resume-section p-3 p-lg-5 d-flex align-items-center">
+                <div class="text-left">
+                    <h2>Head of Recruitment</h2>
+                    <h class = "text-muted">Sigma Eta Pi<br></br>
+                    November 2019 - April 2020
+                    </h>
+                    <p class = "text-muted">
+                    Sigma Eta Pi is the first national professional fraternity that focuses on technology focused entrepreneurship.
+                    </p>
+                    <ul class = "text-muted">
+                        <li>
+                            Marketed our rush, networking and speaker events to strengthen our presence on campus through flyers and social media
+                        </li>
+                        <li>
+                            Organized a startup trek for 40 students. This involved cultivating relationships with 20 startups, and handling travel logistics from Ann Arbor to Chicago for two nights
+                        </li>
+                        <li>
+                            Collaborated and led the entrepreneurial committee to host speakers and hold ideation sessions to increase interest and knowledge in entrepreneurship
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div class="resume-date text-md-right">
-                <span class="text-primary">September 2017 - July 2019</span>
-            </div>
+
+            <div class="resume-section p-3 p-lg-5 d-flex align-items-center">
+                <div class="text-left">
+                    <h2>Makeathon Director and VP Corporate Relations</h2>
+                    <h class = "text-muted">MPowered Entrepreneurship<br></br>
+                    September 2017 - July 2019
+                    </h>
+                    <p class = "text-muted">
+                    Makeathon is a 36-hour long product design competition that empowers students with the mentorship and materials needed to create impactful and innovative products
+                    </p>
+                    <ul class = "text-muted">
+                        <li>
+                            Recruited and led a team of nine students to organize, finance, and market the entire event
+                        </li>
+                        <li>
+                            Sourced and secured sponsorships in excess of $25,000 from 15+ sponsors including Autodesk, National Instruments and General Electric
+                        </li>
+                        <li>
+                            Designed and implemented a case competition, balancing rigor and flexibility to maximize participant engagement. The case is centered around the automotive industry and specifically autonomous vehicles. 
+                        </li>
+                        <li>
+                            Created and put into motion marketing initiatives to attract students of all undergraduate years, as well as master students, through in-person marketing, social media, and press releases
+                        </li>
+                        <li>
+                            Created an interdisciplinary prize to encourage cross-school collaboration and diversification of event participants and products created. Historically, Makeathon has suffered from a lack of representation across schools; 73% of our participants last year were from the college of engineering.
+                        </li>
+                    </ul>
+                </div>
             </div>
         </section>
     }
