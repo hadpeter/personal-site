b611027da8efc3d7ff74c211bde59eac279b554c
hadpeter <hadpeter@umich.edu>
2020-05-18 16:42:10 -0400

    before install react social icons

--- src/components/bio.js
+++ src/components/bio.js
@@ -6,13 +6,24 @@ class Bio extends Component{
         return <div className = "Content">
             <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="bio">
             <h1 class = "text-center">
-                Hadley Peterson
-            </h1>       
+                <big class = "name-title">Hadley Peterson</big>
+            </h1>      
             <p class = "text-center">
             I'm a rising Senior at the University of Michigan,  Ann Arbor, majoring in Computer Science. Outside of coursework I enjoy contributing to several student organizations, meditating, and practicing yoga.
             </p>
+            <div class="social-icons">
+            <a href="https://www.linkedin.com/in/hadley-peterson/">
+                <i class="fab fa-linkedin-in"></i>
+            </a>
+            <a href="https://github.com/hadpeter">
+                <i class="fab fa-github"></i>
+            </a>
+            <a href="https://www.facebook.com/profile.php?id=100009512854778">
+                <i class="fab fa-facebook-f"></i>
+            </a>
+            </div>
             </section>
-            <hr class = "App-hr"></hr>
+            <hr></hr>
         </div>
     }
 }
