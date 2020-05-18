a595caa39a7fc77c107e4116861dabc679004e81
hadpeter <hadpeter@umich.edu>
2020-05-17 20:59:57 -0400

    nav works

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -5,7 +5,7 @@ class Navigation extends Component{
     render(){
         return <div className = "navigation">
             <Container className="p-3">
-            <nav class="sidenav navbar-expand navbar-dark bg-primary fixed-top" id="sideNav">
+            <nav class="sidenav navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
             <a class="navbar-brand js-scroll-trigger" href="#page-top">
                 <span class="d-block d-lg-none">Hadley Peterson</span>
                 <span class="d-none d-lg-block">
@@ -17,6 +17,8 @@ class Navigation extends Component{
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav">
+                <ul class="nav-item">
+                <ul class = "list-unstyled">
                 <li class="nav-item">
                 <a class="nav-link js-scroll-trigger" href="#bio">About</a>
                 </li>
@@ -41,6 +43,8 @@ class Navigation extends Component{
                 <li class="nav-item">
                 <a class="nav-link js-scroll-trigger" href="#interests">Interests</a>
                 </li>
+                </ul>
+                </ul>
             </ul>
             </div>
             </nav>
