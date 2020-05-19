acd9cb36ae2eff39fd24179a0b0b91d24eadfa93
hadpeter <hadpeter@umich.edu>
2020-05-19 12:20:36 -0400

    Updates

---
+++ precache-manifest.b84f4f850f10dd163785a81b731fece4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea0704aaa5739fe17d05f620f7c781b",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "e4e878c6112a675b7373",
+    "url": "/my-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7a940caea35df20f1d35",
+    "url": "/my-app/static/css/main.7a27ceb7.chunk.css"
+  },
+  {
+    "revision": "e4e878c6112a675b7373",
+    "url": "/my-app/static/js/2.5a7a26e3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.5a7a26e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a940caea35df20f1d35",
+    "url": "/my-app/static/js/main.2590234d.chunk.js"
+  },
+  {
+    "revision": "04c7395d6521a83691fa",
+    "url": "/my-app/static/js/runtime-main.344cae30.js"
+  },
+  {
+    "revision": "889eacd72fe524fb3cc070af43e07135",
+    "url": "/my-app/static/media/prof_pic.889eacd7.jpg"
+  }
+]);
