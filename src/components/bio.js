f74443586f42f6d081e46820908a0d174a25197d
hadpeter <hadpeter@umich.edu>
2020-05-19 23:12:18 -0400

    adjusts for mobile view

--- src/components/bio.js
+++ src/components/bio.js
@@ -5,12 +5,12 @@ class Bio extends Component{
     render(){
         return <div className = "Content">
             <section class="resume-section p-3 p-lg-5 d-flex align-items-center" id="bio">
-            <h1 class = "text-center">
-                <big class = "name-title">Hadley Peterson</big>
-            </h1>      
-            <p class = "text-center">
-            I'm a rising Senior at the University of Michigan,  Ann Arbor, majoring in Computer Science. Outside of coursework I enjoy contributing to several student organizations, meditating, and practicing yoga.
-            </p>
+            <div class = "text-center">
+                <h1>Hadley Peterson</h1> 
+                <p class = "bio-blurb">
+                I'm a rising Senior at the University of Michigan,  Ann Arbor, majoring in Computer Science. Outside of coursework I enjoy contributing to several student organizations, meditating, and practicing yoga.
+                </p>
+            </div> 
             </section>
             <div class = "text-center">
                 <SocialIcon url = "https://www.linkedin.com/in/hadley-peterson/" class = "social-icon"></SocialIcon>
