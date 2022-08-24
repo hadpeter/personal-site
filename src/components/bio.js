0a10c57a4cb17f6519fc63e792df9556ec870f5f
hadpeter <hadpeter@umich.edu>
2022-08-23 23:35:11 -0400

    updated bio

--- src/components/bio.js
+++ src/components/bio.js
@@ -8,14 +8,14 @@ class Bio extends Component{
             <div class = "text-center">
                 <h1>Hadley Peterson</h1> 
                 <p class = "bio-blurb">
-                I'm an associate software engineer at Capital One working in the Center for Machine Learning. I recently graduated from the University of Michigan,  Ann Arbor, with a degree in Computer Science from the College of Engineering. Outside work, I enjoy exploring crypto, meditating, and practicing yoga.
+                I'm a senior associate software engineer at Capital One passionate about FinTech, social entrepreneurhsip and cloud technology. I am a University of Michigan grad with a degree in Computer Science from the College of Engineering. Outside work, I enjoy exploring crypto, meditating, and practicing yoga.
                 </p>
             </div> 
             </section>
             <div class = "text-center">
                 <SocialIcon url = "https://www.linkedin.com/in/hadley-peterson/" class = "social-icon"></SocialIcon>
                 <SocialIcon url = "https://github.com/hadpeter" class = "social-icon"></SocialIcon>
-                <SocialIcon url = "https://www.facebook.com/profile.php?id=100009512854778" class = "social-icon"></SocialIcon>
+                <SocialIcon url = "https://hadleypeterson.substack.com/publish?utm_source=menu" class = "social-icon"></SocialIcon>
             </div>
             <hr></hr>
         </div>
