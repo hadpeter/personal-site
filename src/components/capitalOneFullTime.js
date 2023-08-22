fbe03155e096272936ef2460b7d18901ee572e3c
hadpeter <hadpeter@umich.edu>
2023-08-22 08:38:47 -0400

    added recent C1 work

--- src/components/capitalOneFullTime.js
+++ src/components/capitalOneFullTime.js
@@ -9,18 +9,22 @@ class CapitalOneFullTime extends Component{
                 Senior Associate Software Engineer
             </h2>
             <h class = "text-muted">
+                Search Optimization <br></br>
                 Capital One <br></br>
                 July 2022 - Present
             </h>
             <ul class = "text-muted">
-                <li>Added consideration of additional fields in document search. Normalized and boosted various fields to optimize search results.</li>
-                <li>Led several disaster recovery events to show service's resiliency through automatic and manual failovers.</li>
-                <li>Expanded api endpoint functionality to support document creation in search indicies to restore lost data in production.</li>
+                <li>Enabled standardization at the dataset column level to promote reuse and joining of data; updated batch ETL jobs, OpenSearch index and API to support processing, storage, and serving of standardization fields</li>
+                <li>Piloted search evaluation product, winning internal buy-in to develop complete product; onboarded multiple teams to search evaluation product; product was adopted as standard practice in roll out of search changes</li>
+                <li>Enhanced backend to unify datasets using the same underlying data across platforms into a single search result; combining all data platforms containing a datasource users' experience parsing search results</li>
+                <li>Engineered internal tool where data scientists register code snippets with datasets; association between datasets and code snippets is used to onboard team members to data ecosystem </li>
+                <li>Onboarded 30+ new hires to Agile methodologies used at Capital One</li>
             </ul>
             <h2>
                 Associate Software Engineer
             </h2>
             <h class = "text-muted">
+                Machine Learning Platforms <br></br>
                 Capital One <br></br>
                 August 2021 - July 2022
             </h>
